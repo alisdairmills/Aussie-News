6b6ba1a1c2a007df545b469924ebbe3b43aede47
Alex <xander.thompson88@gmail.com>
2021-02-12 10:58:19 +1100

    Initial Commit

---
+++ Feel Good News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Feel Good News
+//
+//  Created by Alexander Thompson on 12/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
