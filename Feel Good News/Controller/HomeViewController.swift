556c6990535d6ab653c09565e8dfe0aebf23ac00
Alex <alexthompsondevelopment@gmail.com>
2021-03-04 15:22:02 +1100

    API and storyboard fix

--- Feel Good News/Controller/HomeViewController.swift
+++ Feel Good News/Controller/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Feel Good News/Model/NewsManager.swift
@@ -0,0 +1,15 @@
+//
+//  NewsManager.swift
+//  Feel Good News
+//
+//  Created by Alexander Thompson on 4/3/21.
+//
+
+import Foundation
+
+struct NewsManager {
+    
+    
+    
+    
+}
