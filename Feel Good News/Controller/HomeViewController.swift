be8c24a1d30e4ef0ae535531bf17fd6c75e297d6
Alex <alexthompsondevelopment@gmail.com>
2021-03-05 15:40:04 +1100

    work on proper JSON implementation

--- Feel Good News/Controller/HomeViewController.swift
+++ Feel Good News/Controller/HomeViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class HomeTableViewController: UITableViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+        NewsManager.shared.getNews { (news) in
+            guard let news = news else {return}
+            print(news[0].title)
+        }
     }
 
 
--- Feel Good News/Model/NewsManager.swift
+++ Feel Good News/Model/NewsManager.swift
@@ -8,8 +8,58 @@
 import Foundation
 
 struct NewsManager {
+
+    let imageCache = NSCache<NSString, NSData>()
+    
+    
+    static let shared = NewsManager()
+    private init() {}
+    
+    let initialUrlString = "https://newsapi.org/v2/"
+    let topHeadlineUS = "top-headlines?country=us"
+    
+    func getNews(with urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                if let data = data {
+                    let newsEnvelope = try? JSONDecoder().decode(NewsEnvelope.self, from: data)
+                   //find a way to decode newsenvelope here in a way I understand. write out a step by step JSon process for what needs to happen
+                }
+            }
+        }
+        
+    }
     
     
     
     
-}
+    
+    
+    //    func getNews(completion: @escaping ([NewsModel]?) -> Void) {
+//        let urlString = "\(initialUrlString)\(topHeadlineUS)&apikey=\(HiddenContent().APIKey)"
+//        guard let url = URL(string: urlString) else {return}
+//
+//        URLSession.shared.dataTask(with: url) { (data, reponse, error) in
+//            guard error == nil, let data = data else {
+//                print(error!)
+//                return
+//            }
+//
+//            let newsEnvelope = try? JSONDecoder().decode(NewsEnvelope.self, from: data)
+//            newsEnvelope == nil ? completion(nil) : completion(newsEnvelope?.articles)
+//            }
+//        .resume()
+//        }
+        
+    
+    }
+
+
+
+
+
