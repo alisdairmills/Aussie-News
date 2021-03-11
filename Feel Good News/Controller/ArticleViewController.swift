81eb1d333ae08e8e8b688f60b8af80dffaba9856
Alex <alexthompsondevelopment@gmail.com>
2021-03-11 15:34:33 +1100

    parse images to tableView

--- Feel Good News/Controller/ArticleViewController.swift
+++ Feel Good News/Controller/ArticleViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ArticleViewController: UIViewController {
 
+    var label: String?
+    
+    @IBOutlet weak var articleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+ 
+        articleLabel.text = label
+      
     }
     
 
--- Feel Good News/View/HomeTableViewCell.swift
+++ Feel Good News/View/HomeTableViewCell.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class HomeTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellTopic: UILabel!
+    @IBOutlet weak var cellDate: UILabel!
     
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var author: UILabel!
-    @IBOutlet weak var cellDescription: UILabel!
     
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
+    override func awakeFromNib() {
+       super.awakeFromNib()
+        
+        cellImage.layer.cornerRadius = cellImage.layer.borderWidth / 5
+  }
+
+   override func setSelected(_ selected: Bool, animated: Bool) {
+       super.setSelected(selected, animated: animated)
+
+      // Configure the view for the selected state
+   }
 
 }
 
